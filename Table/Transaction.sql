44f9d13818c696b72520bf153c5da0b1daff6de3
LUIS <arielrv@ldsbc.edu>
2020-12-09 22:16:14 -0400

    SAMPLE

---
+++ Table/Transaction.sql
@@ -0,0 +1,9 @@
+use DFNB2
+select 
+[branch_id]
+,[acct_id]
+,[tran_date]
+,[tran_type_id]
+,[tran_type_code]
+into Transactio
+from dbo.stg_p2
---
+++ Views/VIEW SAMPLE.sql
@@ -0,0 +1,20 @@
+USE [DFNB2]
+GO
+
+/****** Object:  View [dbo].[Branch_View]    Script Date: 09-Dec-20 22:13:32 ******/
+DROP VIEW [dbo].[Branch_View]
+GO
+
+/****** Object:  View [dbo].[Branch_View]    Script Date: 09-Dec-20 22:13:32 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+create view [dbo].[Branch_View] AS
+select *
+from [dbo].[Branch]
+GO
+
+
